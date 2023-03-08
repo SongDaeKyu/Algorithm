198a74c34513bd279abd5d20f95d03f8dbfc01c9
SongDaeKyu <61207865+SongDaeKyu@users.noreply.github.com>
2023-03-08 19:25:56 +0900

    # 1.1 baekjoon-10250

--- mathematics/baekjoon10250.java
+++ mathematics/baekjoon10250.java
@@ -3,29 +3,53 @@ import java.util.Scanner;
 
 public class baekjoon10250 {
     public static void main(String[] args) {
+    //     #first solve    
+    //     Scanner sc = new Scanner(System.in);
+        
+    //     int T = sc.nextInt();
+    //     int H = 0;
+    //     int W = 0;
+    //     int N = 0;
+        
+    //     for (int i = 0; i < T; i++) {
+    //         int h_count = 0;
+    //         int w_count = 1;
+    //         H = sc.nextInt();
+    //         W = sc.nextInt();
+    //         N = sc.nextInt();
+            
+    //         for (int j = 0; j < N; j++) {
+    //             if (h_count <= H) {
+    //                 h_count++;
+    //                 if (h_count > H) {
+    //                     h_count = 1;
+    //                     w_count += 1;
+    //                 }
+    //             }
+    //         }
+    //         System.out.println(h_count * 100 + w_count);
+    //     }
+    //     sc.close();
+
+    //     #secondsolve
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
         int H = 0;
         int W = 0;
         int N = 0;
+
         for (int i = 0; i < T; i++) {
-            int h_count = 0;
-            int w_count = 1;
             H = sc.nextInt();
             W = sc.nextInt();
             N = sc.nextInt();
-            
-            for (int j = 0; j < N; j++) {
-                if (h_count <= H) {
-                    h_count++;
-                    if (h_count > H) {
-                        h_count = 1;
-                        w_count += 1;
-                    }
-                }
+
+            if(N % H == 0) {
+                System.out.println(H * 100 + N / H);
             }
-            System.out.println(h_count * 100 + w_count);
+            else {
+                System.out.println(N % H * 100 + (N / H + 1));
+            }
         }
         sc.close();
     }
