17d8814fa0546c103400b3346bd344c96cdbb902
SongDaeKyu <61207865+SongDaeKyu@users.noreply.github.com>
2023-03-09 12:50:45 +0900

    update import

--- mathematics/baekjoon10250.java
+++ mathematics/baekjoon10250.java
@@ -1,8 +1,6 @@
 package mathematics;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class baekjoon10250 {
     public static void main(String[] args) throws IOException{
