7d31afd1b2c6679365b14df6a6b79a38473df114
SongDaeKyu <61207865+SongDaeKyu@users.noreply.github.com>
2023-03-12 20:17:36 +0900

    #1.1 baekjoon-2775

--- mathematics/Baekjoon2775.java
+++ mathematics/Baekjoon2775.java
@@ -1,5 +1,6 @@
 package mathematics;
-import java.util.*;
+// import java.util.*;
+import java.io.*;
 
 public class Baekjoon2775 {
     // 1. X (asum 과 sum을 조합하여 규칙찾기)
@@ -27,29 +28,61 @@ public class Baekjoon2775 {
     // }
 
     // 2. 2차원 배열 만들기 (출처: Stranger's LAB)
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
 
-        int[][] arr = new int[15][15];
+    //     int[][] arr = new int[15][15];
         
+    //     for (int i = 0; i < 15; i++) {
+    //         arr[0][i] = i;
+    //         arr[i][1] = 1;
+    //     }
+
+    //     for (int i = 1; i < 15; i++) {
+    //         for (int j = 2; j < 15; j++) {
+    //             arr[i][j] = arr[i-1][j] + arr[i][j-1];
+    //         }
+    //     }
+
+    //     int T  = sc.nextInt();
+
+    //     for (int i = 0; i < T; i++) {
+    //         int k = sc.nextInt();
+    //         int n = sc.nextInt();
+    //         System.out.println(arr[k][n]);
+    //     }
+    //     sc.close();
+    // }
+
+        
+    // 3. 2차원 배열만들기 (메소드, stringbuilder 이용, 출처: Stranger's LAB)
+    public static int[][] arr = new int[15][15];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        make_APT();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(arr[k][n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void make_APT() {
+
         for (int i = 0; i < 15; i++) {
             arr[0][i] = i;
             arr[i][1] = 1;
         }
-
+        
         for (int i = 1; i < 15; i++) {
             for (int j = 2; j < 15; j++) {
                 arr[i][j] = arr[i-1][j] + arr[i][j-1];
             }
         }
-
-        int T  = sc.nextInt();
-
-        for (int i = 0; i < T; i++) {
-            int k = sc.nextInt();
-            int n = sc.nextInt();
-            System.out.println(arr[k][n]);
-        }
-        sc.close();
     }
 }
