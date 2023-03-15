3b6354ec02498c117010a4dc9d6da86316203a79
SongDaeKyu <61207865+SongDaeKyu@users.noreply.github.com>
2023-03-15 15:46:58 +0900

    #1.3 baekjoon-3003

--- mathematics/Baekjoon3003.java
+++ mathematics/Baekjoon3003.java
@@ -1,54 +1,54 @@
 package mathematics;
 import java.util.*;
-import java.io.*;
+// import java.io.*;
 
 public class Baekjoon3003 {
-    // 1
-    // public static void main(String[] args) {
-    //     Scanner sc = new Scanner(System.in);
-    //     int[] A = {1, 1, 2, 2, 2, 8};
-    //     int[] B = new int[6];
+    // 1. 배열로 만들어서 한줄로 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] A = {1, 1, 2, 2, 2, 8};
+        int[] B = new int[6];
         
-    //     for (int i = 0; i < B.length; i++) {
-    //         B[i] = sc.nextInt();
-    //     }
+        for (int i = 0; i < B.length; i++) {
+            B[i] = sc.nextInt();
+        }
 
-    //     sc.close();
+        sc.close();
 
-    //     for (int j = 0; j < B.length; j++) {
-    //         B[j] = A[j] - B[j];
-    //     }
+        for (int j = 0; j < B.length; j++) {
+            B[j] = A[j] - B[j];
+        }
 
-    //     for (int k = 0; k < B.length; k++) {
-    //         System.out.println(B[k]);
-    //     }
-    // }
+        for (int k = 0; k < B.length; k++) {
+            System.out.print(B[k] + " ");
+        }
+    }
 
     // 2. 한 줄로 출력하기 위해 print 사용 (출처: Stranger's LAB)
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // public static void main(String[] args) throws IOException{
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int king = 1;
-        int queen = 1;
-        int rook = 2;
-        int bishop = 2;
-        int knight = 2;
-        int pawn = 8;
+    //     int king = 1;
+    //     int queen = 1;
+    //     int rook = 2;
+    //     int bishop = 2;
+    //     int knight = 2;
+    //     int pawn = 8;
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    //     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        king = king - Integer.parseInt(st.nextToken());
-        queen = queen - Integer.parseInt(st.nextToken());
-        rook = rook - Integer.parseInt(st.nextToken());
-        bishop = bishop - Integer.parseInt(st.nextToken());
-        knight = knight - Integer.parseInt(st.nextToken());
-        pawn = pawn - Integer.parseInt(st.nextToken());
+    //     king = king - Integer.parseInt(st.nextToken());
+    //     queen = queen - Integer.parseInt(st.nextToken());
+    //     rook = rook - Integer.parseInt(st.nextToken());
+    //     bishop = bishop - Integer.parseInt(st.nextToken());
+    //     knight = knight - Integer.parseInt(st.nextToken());
+    //     pawn = pawn - Integer.parseInt(st.nextToken());
     
-        System.out.print(king + " ");
-        System.out.print(queen + " ");
-        System.out.print(rook + " ");
-        System.out.print(bishop + " ");
-        System.out.print(knight + " ");
-        System.out.print(pawn + " ");
-    }
+    //     System.out.print(king + " ");
+    //     System.out.print(queen + " ");
+    //     System.out.print(rook + " ");
+    //     System.out.print(bishop + " ");
+    //     System.out.print(knight + " ");
+    //     System.out.print(pawn + " ");
+    // }
 }
